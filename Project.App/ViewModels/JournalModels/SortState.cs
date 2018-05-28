408385ea4f2375f134624257e8859e08a4092c81
spike91 <emlecap7@mail.ru>
2018-05-28 03:05:25 +0300

    journal pagination

---
+++ Project.App/ViewModels/JournalModels/SortState.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Project.App.ViewModels.JournalModels
+{
+    public enum SortState
+    {
+        TitleAsc,   
+        TitleDesc,   
+        LanguageAsc, 
+        LanguageDesc,   
+        CompanyAsc, 
+        CompanyDesc,
+        CategoryAsc, 
+        CategoryDesc 
+    }
+}
